4a8ae11a0536edecf28238c9d33e8b428280dafb
Utopyx <30875147+Utopyx@users.noreply.github.com>
2021-12-13 15:30:56 +0100

    grill still wip

--- classes/Case.py
+++ classes/Case.py
@@ -1,4 +1,4 @@
 class Case :
-    def __init__(this,pos,value) -> None:
+    def __init__(this, pos, value) -> None:
         this.position = pos
         this.value = value
--- classes/Grille.py
+++ classes/Grille.py
@@ -1,13 +1,21 @@
+from Case import Case
+
+genius = [8, 1, 6, 3, 5, 7, 4, 9, 2]
 class Grille:
   def __init__(this):
-    this.tableau = []
+    this.tableau = [Case() for k in range(9)]
 
   def isCaseEmpty(this, pos):
     return this.tableau[pos].value == None
 
-  def changeCase(this, pos, val):
+  def updateCase(this, pos, val):
     if this.isCaseEmpty(pos):
       this.tableau[pos].value = val
 
-  def checkGameState(this):
-    
+  def isGameOver(this):
+    # brute force à améliorer si possible pour le rendre plus sexy
+    combinaisons = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+  
+    for comb in combinaisons:
+      if this.tableau[comb[2]].value == this.tableau[comb[1]].value and this.tableau[comb[1]].value == this.tableau[comb[0]].value:
+        return True
