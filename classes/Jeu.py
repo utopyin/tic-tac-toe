f8206e01af0fdf009d730b2ee4017505f7a92731
PouletRaPHOO <70721948+PouletRaPHOO@users.noreply.github.com>
2021-12-13 15:27:39 +0100

    Jeu is finished mofo
    ACAB

--- classes/Jeu.py
+++ classes/Jeu.py
@@ -1,8 +1,38 @@
 from typing import List
 
 
-class jeu:
-    def __init__(this, players:List) -> None:
+class Jeu:
+    def __init__(this, players:List, grid) -> None:
         this.players = players
         this.count= 0
-        this.index = 1
+        this.index = 0
+        this.grid = grid
+
+    def whoplays(this):
+        return this.players[this.index]
+
+    def nextplayer(this) :
+        this.index = (this.index+1)%2
+
+    def round(this) :
+        print(this.grid)
+        activeplayer = this.whoplays()
+        casechosen= int(input("Veuillez choisir la case où jouer"))
+        while casechosen<0 or casechosen>8 or not(this.grid.isCaseEmpty(casechosen)) :
+            casechosen= int(input("Veuillez choisir la case où jouer"))
+        this.grid.updateCase(casechosen,activeplayer.symbole)
+        this.count += 1
+
+    def game(this) :
+        while True :
+            this.round()
+            if this.count>=5 :
+                over = this.grid.isGameOver()
+                if over :
+                    print
+                    return 
+            if this.count>=9:
+                print("Egalité")
+                return 0
+            this.nextplayer()
+
