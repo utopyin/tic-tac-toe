4c1b4ec3c725617a1a8a9aee0043304d72d4c33a
Utopyx <30875147+Utopyx@users.noreply.github.com>
2021-12-14 09:30:38 +0100

    juste j'ai modif la case chosen

--- python/classes/Jeu.py
+++ python/classes/Jeu.py
@@ -20,8 +20,8 @@ class Jeu:
     def round(this):
         print(this.grid)
         activePlayer = this.whoPlays()
-        caseChosen= int(input("Veuillez choisir la case où jouer : "))
-        while caseChosen<0 or caseChosen>8 or not(this.grid.isCaseEmpty(caseChosen)):
+        caseChosen = int(input("Veuillez choisir la case où jouer : ")) - 1
+        while caseChosen < 0 or caseChosen > 8 or not(this.grid.isCaseEmpty(caseChosen)):
             caseChosen = int(input("Veuillez choisir la case où jouer : "))
         this.grid.updateCase(caseChosen, activePlayer.symbole)
         this.count += 1
@@ -29,12 +29,13 @@ class Jeu:
     def game(this):
         while True:
             this.round()
-            if this.count>=5:
-                over = this.grid.isGameOver()
-                if over:
+            if this.count >= 5:
+                if this.grid.isGameOver():
+                    print(this.grid)
                     print("Le joueur " + str(this.Joueurs[this.index].nom) + " a gagné !")
                     return 1
-            if this.count>=9:
+            if this.count >= 9:
+                print(this.grid)
                 print("Egalité")
                 return 0
             this.nextPlayer()
