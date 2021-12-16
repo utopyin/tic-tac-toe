1103847ba08d488ebe0ac771feafbe8028a3a082
Utopyx <30875147+Utopyx@users.noreply.github.com>
2021-12-16 16:14:57 +0100

    notifications wip

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import WSProvider from './modules/ws/ws';
+import NotisProvider from './modules/notifications/noti';
 
 ReactDOM.render(
   <React.StrictMode>
-    <WSProvider>
-      <App />
-    </WSProvider>
+    <NotisProvider>
+      <WSProvider>
+        <App />
+      </WSProvider>
+    </NotisProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
